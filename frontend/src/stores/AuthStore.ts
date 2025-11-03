177c74d8e3dcc1b4748898ca3a358a053199c2c8
Clarence Mauricio <128758441+FreezingFruit@users.noreply.github.com>
2025-11-04 06:31:05 +0800

    added interfaces for user and task

---
+++ frontend/src/stores/AuthStore.ts
@@ -0,0 +1,14 @@
+import type { User } from '@/types/User'
+import { defineStore } from 'pinia'
+
+interface AuthState {
+  user: User | null
+  token: string | null
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: (): AuthState => ({
+    user: null,
+    token: localStorage.getItem('token'),
+  }),
+})
---
+++ frontend/src/types/Task.ts
@@ -0,0 +1,7 @@
+export interface Task {
+  _id?: string
+  title?: string
+  isDone?: boolean
+  userId?: string
+  createdAt?: string
+}
