eaba591d2c563c9786a1ed02473e5d085cbac742
Clarence Mauricio <128758441+FreezingFruit@users.noreply.github.com>
2025-11-04 05:28:26 +0800

    added auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,23 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../models/User.js";
 
-cosnt JWT_SECRET = process.env.JWT_SECRET || 'incase-jwt-breaks-for-testing-only'
+const JWT_SECRET =
+  process.env.JWT_SECRET || "incase-jwt-breaks-for-testing-only";
+
+export const authenticateToken = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer", "");
+
+    if (!token) {
+      return res.status(401).json({ error: "No token!" });
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    req.userId = decoded.userId;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid Token" });
+  }
+};
